7663258a491c5c974d8395700c7d3ed1c953409a
Nijimingcheng <1936762551@qq.com>
2024-06-13 05:24:11 -0300

    add function:readcallback_

--- Project/Channel.cpp
+++ Project/Channel.cpp
@@ -73,3 +73,78 @@ uint32_t Channel::revents()
 {
     return revents_;
 }
+
+void Channel::handleevent()
+{
+    //第一种：对方已关闭，有些系统检测不到，可以使用EPOLLIN，recv()返回0。
+    if (revents_ & EPOLLRDHUP)
+    {
+        printf("client(eventfd=%d) disconnected.)\n", fd_);
+        close(fd_);            // 关闭客户端的fd。
+    }
+    //第二种：接收缓冲区中有数据可以读。
+    else if (revents_ & (EPOLLIN|EPOLLPRI))
+    {                         //普通数据 | 带外数据
+        readcallback_();
+    }
+    //第三种：有数据需要写，暂时没有代码，以后再说。
+    else if (revents_ & EPOLLOUT)
+    {
+    }
+    else //第四种：其它事件，都视为错误。
+    {
+        printf("client(eventfd=%d) error.\n",fd_);
+        close(fd_);            // 关闭客户端的fd。
+    }
+}
+//处理新客户端连接请求
+void Channel::newconnection(Socket* serversock)
+{
+    INETAddress clientaddr;
+    //注意, clientsock只能new出来,不能在栈上, 否则析构函数会关闭fd。
+    //还有, 这里new出来的对象没有释放, 这个问题以后再解决
+    Socket *clientsock = new Socket(serversock->accept(clientaddr));
+
+    printf ("accept client(fd=%d,ip=%s,port=%d) ok.\n",clientsock->fd(), clientaddr.ip(), clientaddr.port()); //inet_ntoa函数是将二进制的网络ip转换成十分点进制的网络ip
+
+    // 为新客户端连接准备读事件，并添加到epoll中。
+    Channel *clientchannel = new Channel(ep_, clientsock->fd());   // 这里new出来的对象没有释放，这个问题以后再解决。
+    clientchannel->setreadcallback(std::bind(&Channel::onmessage, clientchannel));
+    clientchannel->useet();                                       // 客户端连上来的fd采用边缘触发。
+    clientchannel->enablereading();                               // 让epoll_wait()监视clientchannel的读事件。
+}
+//处理对端发过来的消息
+void Channel::onmessage()
+{
+    char buffer[1024];
+    while (true)             // 由于使用非阻塞IO，一次读取buffer大小数据，直到全部的数据读取完毕。
+    {
+        bzero(&buffer, sizeof(buffer));
+        ssize_t nread = read(fd_, buffer, sizeof(buffer));    // 这行代码用了read()，也可以用recv()，一样的，不要纠结。
+        if (nread > 0)      // 成功的读取到了数据。
+        {
+            // 把接收到的报文内容原封不动的发回去。
+            printf("recv(eventfd=%d):%s\n", fd_, buffer);
+            send(fd_, buffer, strlen(buffer),0);
+        }
+        else if (nread == -1 && errno == EINTR) // 读取数据的时候被信号中断，继续读取。
+        {
+            return;
+        }
+        else if (nread == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))) // 全部的数据已读取完毕。
+        {
+            return;
+        }
+        else if (nread == 0)  // 客户端连接已断开。
+        {
+            printf("client(eventfd=%d) disconnected.\n",fd_);
+            close(fd_);            // 关闭客户端的fd。
+            return;
+        }
+    }
+}
+//设置fd_读事件的回调函数
+void Channel::setreadcallback(std::function<void()> rb)
+{
+    readcallback_ = rb;
+}
--- Project/Channel.h
+++ Project/Channel.h
@@ -2,31 +2,38 @@
 #define __CHANNEL_H__
 
 #include <sys/epoll.h>
+#include <functional>
 #include "Epoll.h"
+#include "INETAddress.h"
+#include "Socket.h"
 
 class Epoll;
 
 class Channel
 {
 private:
-    int fd_ = -1;   //channel拥有的fd(SOCKET句柄), Channel和fd是一对一的关系。
-    Epoll *ep_ = nullptr;   //Channel对应的红黑树, Channel与Epoll是多对一的关系, 一个Channel只对应一个Epoll。
-    bool inepoll_ = false;  //Channel是否已添加到epoll树上, 如果未添加, 调用epoll_ctl()的时候用EPOLL_CTL_ADD, 否则用EPOLL_CTL_MOD。
-    uint32_t events_ = 0;   //fd_需要监视的事件。listenfd和clientfd需要监视EPOLLIN(读事件), clientfd还可能需要监视EPOLLOUT(写事件)。
-    uint32_t revents_ = 0;  //fd_已经发生的事件。
+    int fd_ = -1;                                 //channel拥有的fd(SOCKET句柄), Channel和fd是一对一的关系。
+    Epoll *ep_ = nullptr;                         //Channel对应的红黑树, Channel与Epoll是多对一的关系, 一个Channel只对应一个Epoll。
+    bool inepoll_ = false;                        //Channel是否已添加到epoll树上, 如果未添加, 调用epoll_ctl()的时候用EPOLL_CTL_ADD, 否则用EPOLL_CTL_MOD。
+    uint32_t events_ = 0;                         //fd_需要监视的事件。listenfd和clientfd需要监视EPOLLIN(读事件), clientfd还可能需要监视EPOLLOUT(写事件)。
+    uint32_t revents_ = 0;                        //fd_已经发生的事件。
+    std::function<void()> readcallback_;          //fd_读事件的回调函数
 public:
-    Channel(Epoll* ep, int fd);         //构造函数
-    ~Channel();                         //析构函数
+    Channel(Epoll* ep, int fd);    //构造函数
+    ~Channel();                                    //析构函数
 
-    int fd();                           //返回fd_成员
-    void useet();                       //采用边缘触发
-    void enablereading();               //让epoll_wait()监视fd_的读事件
-    void setinepoll();                  //把inepoll_成员的值设置为true
-    void setrevents(uint32_t ev);       //设置revents_成员的值为参数ev
-    bool inpoll();                      //返回inepoll_成员
-    uint32_t events();                  //返回events_成员
-    uint32_t revents();                 //返回revents_成员
+    int fd();                                      //返回fd_成员
+    void useet();                                  //采用边缘触发
+    void enablereading();                          //让epoll_wait()监视fd_的读事件
+    void setinepoll();                             //把inepoll_成员的值设置为true
+    void setrevents(uint32_t ev);                  //设置revents_成员的值为参数ev
+    bool inpoll();                                 //返回inepoll_成员
+    uint32_t events();                             //返回events_成员
+    uint32_t revents();                            //返回revents_成员
+    void handleevent();         //事件处理函数, epoll_wait()
+    void newconnection(Socket* serversock);        //处理新客户端连接请求
+    void onmessage();                              //处理对端发过来的消息
+    void setreadcallback(std::function<void()> rb);//设置fd_读事件的回调函数
 };
 
 #endif
-
